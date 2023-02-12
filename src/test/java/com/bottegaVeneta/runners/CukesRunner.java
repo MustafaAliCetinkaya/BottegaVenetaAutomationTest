52ff491e1901695e708b28e2d3878e5a4232cdb1
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-13 01:15:10 +0300

    TC02 is WIP

--- src/test/java/com/bottegaVeneta/runners/CukesRunner.java
+++ src/test/java/com/bottegaVeneta/runners/CukesRunner.java
@@ -4,13 +4,19 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+/*- Dryrun: turns on and off the running of step_definitions.
+- If I don't want to actually run my code, and just want to get my snippets for implementing, I will turn dryRun on by giving "true" value
+dryRun = true --> do not run my step_definitions, but only check if any step is missing snippet
+dryRun = false --> this will turn off dryRun, and our code will run as expected.
+*/
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        dryRun = false,
-        tags = "@TC01"
+        tags = "@TC01",
+        dryRun = false          //False-->Means directly start the tests / True-->Check for the missing methods, do not run
 )
+
 public class CukesRunner {
+
 }
