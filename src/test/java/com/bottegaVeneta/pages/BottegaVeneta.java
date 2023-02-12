2da370e7f44780f083b80e44c8070b55d8220dae
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-13 00:06:02 +0300

    TC01 is WIP

--- src/test/java/com/bottegaVeneta/pages/BottegaVeneta.java
+++ src/test/java/com/bottegaVeneta/pages/BottegaVeneta.java
@@ -1,13 +1,16 @@
 package com.bottegaVeneta.pages;
 
 import com.bottegaVeneta.utilities.Driver;
+import com.bottegaVeneta.utilities.ReusableMethods;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class BottegaVeneta {
     public BottegaVeneta() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(css = "button#onetrust-accept-btn-handler")
@@ -16,8 +19,51 @@ public class BottegaVeneta {
     public WebElement leftHandSideMenuButton;
     @FindBy(xpath = "//span[contains(text(),'My Account')]")
     public WebElement myAccountButton;
+    @FindBy(xpath = "//*[contains(text(),'Create my profile')]")
+    public WebElement createMyProfileButton;
+    @FindBy(css = "h1.c-registerinfo__headertitle")
+    public WebElement createMyProfileText;
     @FindBy(css = "div.c-card__header.c-card__header--createaccount h1.c-card__title")
     public WebElement newCustomerText;
+    @FindBy(css = "input#dwfrm_profile_customer_civility_customer0")
+    public WebElement titleRadioButton;
+    @FindBy(xpath = "//*[contains(text(),'First name')]")
+    public WebElement firstNameBox;
 
+    public void fillRequiredFields() {
+        ReusableMethods.jsScrollClick(titleRadioButton);
+        ReusableMethods.getActions().click(firstNameBox)
+                .sendKeys(ReusableMethods.getFaker().name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(ReusableMethods.getFaker().name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(ReusableMethods.getFaker().number().numberBetween(1, 31) + ""+ReusableMethods.getFaker().number().numberBetween(1, 12) + ""+ReusableMethods.getFaker().number().numberBetween(1900, 2023) + "")
+                .sendKeys(Keys.TAB)
+                .sendKeys(ReusableMethods.getFaker().internet().emailAddress())
+                .sendKeys(Keys.TAB)
+                .sendKeys(ReusableMethods.getFaker().internet().password(12, 16, true, true, true)).perform();
+
+    }
+
+    @FindBy(css = "select.c-form__select")
+    public WebElement selectBox;
+
+    public void setSelectCountryDropdown() {
+        Select selectCountryDropdown=new Select(selectBox);
+        selectCountryDropdown.selectByIndex(11);
+        ReusableMethods.jsScrollClick(privacyPolicyBox);
+        ReusableMethods.jsScrollClick(signupNewsletterBox);
+    }
+
+    @FindBy(xpath = "//*[contains(text(),'I have read and understood')]")
+    public WebElement privacyPolicyBox;
+    @FindBy(xpath = "//*[contains(text(),'I confirm that I am at least 18')]")
+    public WebElement ageConfirmationBox;
+    @FindBy(xpath = "//*[contains(text(),'I would like to receive updates about Bottega Veneta new activities')]")
+    public WebElement signupNewsletterBox;
+    @FindBy(xpath = "//*[contains(text(),'Request Received')]")
+    public WebElement requestReceivedWindow;
+    @FindBy(xpath = "(//button[@data-ref=\"closePopinTrigger\"])[11]")
+    public WebElement closeRequestReceivedWindow;
 
 }
--- src/test/java/com/bottegaVeneta/stepDefinitions/TC01_StepDefinitions.java
+++ src/test/java/com/bottegaVeneta/stepDefinitions/TC01_StepDefinitions.java
@@ -1,7 +1,6 @@
 package com.bottegaVeneta.stepDefinitions;
 
 import com.bottegaVeneta.pages.BottegaVeneta;
-import com.bottegaVeneta.utilities.BrowserUtils;
 import com.bottegaVeneta.utilities.ConfigurationReader;
 import com.bottegaVeneta.utilities.Driver;
 import com.bottegaVeneta.utilities.ReusableMethods;
@@ -23,6 +22,12 @@ public class TC01_StepDefinitions {
         Assert.assertEquals("Bottega Veneta® GB | Official Website", Driver.getDriver().getTitle());
     }
 
+    @And("Click on Go to accept all cookies button")
+    public void clickOnGoToInternationalVersionButton() {
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.jsScrollClick(pageObject.acceptAllCookiesButton);
+    }
+
     @Then("Click on my account button from the left hand side menu")
     public void click_on_my_account_button_from_the_left_hand_side_menu() {
         ReusableMethods.jsScrollClick(pageObject.leftHandSideMenuButton);
@@ -32,72 +37,60 @@ public class TC01_StepDefinitions {
 
     @Then("Verify New Customer is visible")
     public void verify_new_customer_is_visible() {
+        ReusableMethods.jsScroll(pageObject.newCustomerText);
         Assert.assertTrue(pageObject.newCustomerText.isDisplayed());
     }
 
     @Then("Click on the create my profile button")
     public void click_on_the_create_my_profile_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.jsScrollClick(pageObject.createMyProfileButton);
     }
 
     @Then("Verify that Create my profile is visible")
     public void verify_that_create_my_profile_is_visible() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertEquals(pageObject.createMyProfileText.getText(),"Create my profile");
     }
 
     @Then("Fill all the required fields")
     public void fill_all_the_required_fields() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        pageObject.fillRequiredFields();
+        pageObject.setSelectCountryDropdown();
     }
 
     @Then("Select checkbox Sign up for our newsletter!")
     public void select_checkbox_sign_up_for_our_newsletter() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.jsScrollClick(pageObject.ageConfirmationBox);
     }
 
     @Then("Click Create My Profile button")
     public void click_create_my_profile_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.jsScrollClick(pageObject.createMyProfileButton);
     }
 
     @Then("Verify that Request Received is visible")
     public void verify_that_request_received_is_visible() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.jsScrollClick(pageObject.requestReceivedWindow);
+        Assert.assertTrue(pageObject.requestReceivedWindow.isDisplayed());
     }
 
     @Then("Click Check my mailbox to activate my account button")
     public void click_check_my_mailbox_to_activate_my_account_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("Close the Request Received window")
     public void close_the_request_received_window() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("Back to the homepage")
     public void back_to_the_homepage() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("User should be able to see DISCOVER THE NEW ANDIAMO BAG text in the lower left corner")
     public void user_should_be_able_to_see_discover_the_new_andiamo_bag_text_in_the_lower_left_corner() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
-    @And("Click on Go to accept all cookies button")
-    public void clickOnGoToInternationalVersionButton() {
-        Driver.getDriver().navigate().refresh();
-        ReusableMethods.jsScrollClick(pageObject.acceptAllCookiesButton);
-    }
 }
