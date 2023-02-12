735fc72dd7455c5845b382aba447da9bc581de55
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-13 01:02:31 +0300

    TC02 is WIP

--- src/test/java/com/bottegaVeneta/runners/CukesRunner.java
+++ src/test/java/com/bottegaVeneta/runners/CukesRunner.java
@@ -6,18 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {
-                //"pretty",
-                "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt",
-                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
-    },
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "stepDefinitions",
         dryRun = false,
-        tags = "TC01",
-        publish = false
-
+        tags = "@TC01"
 )
 public class CukesRunner {
 }
