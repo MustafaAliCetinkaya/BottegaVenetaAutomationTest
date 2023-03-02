2dc4ce5775869e4dc10359f867d6db3257b132b8
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-03 00:45:05 +0300

    AgileProCRM tests are WIP

--- src/test/java/com/bottegaVeneta/runners/CukesRunner.java
+++ src/test/java/com/bottegaVeneta/runners/CukesRunner.java
@@ -4,11 +4,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-/*- Dryrun: turns on and off the running of step_definitions.
-- If I don't want to actually run my code, and just want to get my snippets for implementing, I will turn dryRun on by giving "true" value
-dryRun = true --> do not run my step_definitions, but only check if any step is missing snippet
-dryRun = false --> this will turn off dryRun, and our code will run as expected.
-*/
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
@@ -17,7 +12,8 @@ dryRun = false --> this will turn off dryRun, and our code will run as expected.
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
         },
-        features = "src/test/resources/features/crm",
+        features = "/Users/mustafacetinkaya/IdeaProjects/BottegaVenetaAutomationTest/src/test/resources/features/crm",
+        glue = "com/bottegaVeneta/stepDefinitions",
         publish = true //will enable a functionality from cucumber to generate a public daily link for the report of our final execution of our code
 
 )
