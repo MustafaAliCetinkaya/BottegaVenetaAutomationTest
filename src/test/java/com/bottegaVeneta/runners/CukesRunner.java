3299a8b532db1fbd431b8c24f363c674c8850520
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-03 09:25:42 +0300

    Framework is updated

--- src/test/java/com/bottegaVeneta/runners/CukesRunner.java
+++ src/test/java/com/bottegaVeneta/runners/CukesRunner.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt",
         },
         features = "src/test/resources/features/crm",
+        glue = "com/bottegaVeneta/stepDefinitions",
         publish = true //will enable a functionality from cucumber to generate a public daily link for the report of our final execution of our code
 
 )
--- src/test/java/com/bottegaVeneta/utilities/Driver.java
+++ src/test/java/com/bottegaVeneta/utilities/Driver.java
@@ -33,7 +33,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.202.197.211";
+                        String gridAddress = "44.199.250.13";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
