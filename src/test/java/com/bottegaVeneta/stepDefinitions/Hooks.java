4a0880a5c732435e1794e3af4eb64edd8ea2905c
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-16 23:14:32 +0300

    AgileProCRM tests are WIP

--- src/test/java/com/bottegaVeneta/stepDefinitions/Hooks.java
+++ src/test/java/com/bottegaVeneta/stepDefinitions/Hooks.java
@@ -5,7 +5,6 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-
 public class Hooks {
     @After
     public void tearDown(Scenario scenario) {
